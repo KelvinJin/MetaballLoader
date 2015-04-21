217bd56b9e690db64696a65038b2ead81cedf925
KelvinJin <owenwjowenwj@gmail.com>
2015-04-21 23:03:23 +1000

    Initial Commit

---
+++ MetaSpinExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MetaSpinExample
+//
+//  Created by Jin Wang on 21/04/2015.
+//  Copyright (c) 2015 uthoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
